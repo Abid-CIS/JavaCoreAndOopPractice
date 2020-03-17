e735ca4a8baeb83f9433935fc18e767095b39561
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-03-17 23:59:45 +0600

    All programs are about java core and OOP practice

---
+++ src/Nested_For_loop/Nested_For_Loop_Demo.java
@@ -0,0 +1,19 @@
+
+package Nested_For_loop;
+
+
+public class Nested_For_Loop_Demo {
+    
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=100; i++){
+            for(int j =1; j<=100; j++){
+                
+             System.out.println("Apurba");
+            
+            }
+            
+            System.out.println("The amin culpit is there");
+        }
+    }
+}
