b0ee1b57b03f76bc2197c8fae3610ea27620ceb6
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-03-31 17:20:10 +0600

    All programs are about java core and OOP practice

--- src/Input_Java/InputDemoFloat.java
+++ src/Input_Java/InputDemoFloat.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Input_Java;
 
-/**
- *
- * @author user
- */
+import java.util.Scanner;
+
+
 public class InputDemoFloat {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first float number");
+        float A = input.nextFloat();
+        System.out.println("The first value is: " + A);
+        
+        System.out.println("Enter the first float number");
+        float B = input.nextFloat();
+        System.out.println("The first value is: " + A);
+        
+        float C = A+B;
+        System.out.println("The sum is: " + C);
+        
+        String s1 = String.valueOf(A) + "+" + Float.toString(B);
+        System.out.println("Conversion from float to String: " + s1);
+    }
 }
