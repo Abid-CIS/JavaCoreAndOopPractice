1eb0bf23b926dcba19b3aefe8266d2c91fca142b
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 01:28:56 +0600

    All programs are about java core and OOP practice

---
+++ src/LoopPracticeDemo2/Namta.java
@@ -0,0 +1,20 @@
+
+package LoopPracticeDemo2;
+
+import java.util.Scanner;
+
+
+public class Namta {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the value you want");
+        int n = input.nextInt();
+        int multiply = 1;
+        for(int i =1; n >= i; i++){
+            multiply = n*i;
+            System.out.println( n + " X " + i + " = " + multiply);
+        }
+        
+    }
+}
