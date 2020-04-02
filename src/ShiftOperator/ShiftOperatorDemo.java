8fee7147c62f6980a6d88d2634363f5662d2fb97
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-02 21:52:33 +0600

    All programs are about java core and OOP practice

--- src/ShiftOperator/ShiftOperatorDemo.java
+++ src/ShiftOperator/ShiftOperatorDemo.java
@@ -5,6 +5,13 @@ package ShiftOperator;
 public class ShiftOperatorDemo {
     
     public static void main(String[] args) {
+        //left shift
+        int a = 32;
+        int b = 16;
+        int c = a >> 2;// Right shift means 2 dia 2 bar vag(2 die 2 bar vag)
+        int d = a << 3; // Left shift means 32 *2*2*2(2 die 3 bar gun)
         
+        System.out.println("The value c : "+ c );
+        System.out.println("The value c : "+ d );
     }
 }
