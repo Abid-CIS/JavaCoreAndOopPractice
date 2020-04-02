8e6ce0bc28a9a17123fd84cca2f1f317f3b79687
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-02 22:23:58 +0600

    All programs are about java core and OOP practice

---
+++ src/ShiftOperator/ShiftOperatorDemo3.java
@@ -0,0 +1,15 @@
+
+package ShiftOperator;
+
+
+public class ShiftOperatorDemo3 {
+    
+    public static void main(String[] args) {
+        int w = 16;
+        int y = (w >> 3);// 10 k 3 bar 2 dara kromannoye vag kora..//8, 4, 2// 2
+        System.out.println("y " + y);
+        
+        
+        
+    }
+}
