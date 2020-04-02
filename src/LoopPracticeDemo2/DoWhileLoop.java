c95cbcca5dcdc2a3acfb1c054c7fca23b8fac50f
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 00:40:18 +0600

    All programs are about java core and OOP practice

---
+++ src/LoopPracticeDemo2/DoWhileLoop.java
@@ -0,0 +1,25 @@
+
+package LoopPracticeDemo2;
+
+import java.util.Scanner;
+
+
+public class DoWhileLoop {
+    
+    public static void main(String[] args) {
+     
+     Scanner input = new Scanner(System.in); 
+     System.out.println("Enter a value of n: ");
+     int n = input.nextInt();
+     int i=1;
+     int count = 1;
+     do{
+     
+         System.out.print(i + " * \b" );
+         count = count*i;//2
+         System.out.println("The count is: " + count);
+         i++;//2
+     }while(i <= n);
+        
+    }
+}
