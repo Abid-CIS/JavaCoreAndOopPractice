ccb8b91780aa5c92e9b6eca4aab7e160d95bca79
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 02:27:11 +0600

    All programs are about java core and OOP practice

---
+++ src/LoopPracticeDemo2/RowCloumn.java
@@ -0,0 +1,15 @@
+
+package LoopPracticeDemo2;
+
+
+public class RowCloumn {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i= i+1){
+            for(int j = 1; j <=4 ; j++){
+                System.out.println("Row No: " + i + " of " + "column "+j);
+            }
+            System.out.print("\n");
+            
+        }
+    }
+}
