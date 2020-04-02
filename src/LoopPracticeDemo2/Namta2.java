f558959982f1421fe98ae557a53919c5d92e6911
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 01:50:35 +0600

    All programs are about java core and OOP practice

---
+++ src/LoopPracticeDemo2/Namta2.java
@@ -0,0 +1,19 @@
+
+package LoopPracticeDemo2;
+
+import java.util.Scanner;
+
+
+public class Namta2 {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the initial number");
+        int i = input.nextInt();
+        System.out.println("Enter the final number");
+        int f = input.nextInt();
+        
+    }
+}
