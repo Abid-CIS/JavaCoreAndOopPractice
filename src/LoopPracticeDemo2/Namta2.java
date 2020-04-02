b3c9d32b7eaf192d5c23323c395a7ffecfedf75d
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 02:05:00 +0600

    All programs are about java core and OOP practice

--- src/LoopPracticeDemo2/Namta2.java
+++ src/LoopPracticeDemo2/Namta2.java
@@ -11,9 +11,19 @@ public class Namta2 {
         Scanner input = new Scanner(System.in);
         
         System.out.println("Enter the initial number");
-        int i = input.nextInt();
+        int m = input.nextInt();
         System.out.println("Enter the final number");
-        int f = input.nextInt();
+        int n = input.nextInt();
+        int multi = 1;
+        
+        for(int i=m; i <= n; i++){
+            
+            for (int j = 1; j <= 10; j++) {
+                multi = i*j;
+                System.out.println( i +" X "+ j + " = " + multi);
+            }
+            System.out.println("\n\n");
+        }
         
     }
 }
