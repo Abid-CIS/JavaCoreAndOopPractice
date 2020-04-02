5cf471a3ec1a37b16422ee9dcb6d18e0fb42a5e0
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-02 20:29:44 +0600

    All programs are about java core and OOP practice

---
+++ src/BitwiseOperator/BitwiseOperatorAnd.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BitwiseOperator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class BitwiseOperatorAnd {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+        
+        System.out.print("Enter the first digit: \n");
+        a = input.nextInt();
+        System.out.print("Enter the second digit: \n");
+        b = input.nextInt();
+        
+        c = a & b;
+        // here ( a integer and b also integer )
+        /*
+        At first, 
+        a will converted to binary number ;
+        b will also converted to binary number;
+         than a(binary formate) will multiply with b(binary formate) 
+        then retuirned binary number will converted to decimal format 
+        and it will display as a result.
+        */
+        // and done multiply between binary numbers.
+        System.out.println("A Bitwise B: c is-> " + c);
+    }
+}
