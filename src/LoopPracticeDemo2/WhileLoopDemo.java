90c1ffc4ac33af12eed1627a8459db84355cf678
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 00:17:23 +0600

    All programs are about java core and OOP practice

--- src/LoopPracticeDemo2/WhileLoopDemo.java
+++ src/LoopPracticeDemo2/WhileLoopDemo.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package LoopPracticeDemo2;
 
-/**
- *
- * @author user
- */
+package LoopPracticeDemo2;
+import java.util.Scanner;
+
+
 public class WhileLoopDemo {
     
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number to show it's factorial!");
+        int n = input.nextInt();
+        int count = 1;
+        int i = 1;
+        while(n >= i){
+            System.out.println("The value is: " + i);
+            count = count*i;
+            i++;
+            
+        }
+        System.out.println("The factorial of " + n +" : " + count);
+    }
 }
