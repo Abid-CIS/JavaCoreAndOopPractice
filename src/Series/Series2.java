263e0c0b4d831d2b3a25cda320f3101a09e5da22
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-03 15:12:49 +0600

    All programs are about java core and OOP practice

---
+++ src/Series/Series2.java
@@ -0,0 +1,13 @@
+
+package Series;
+
+import java.util.Scanner;
+
+
+public class Series2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+ 
+}
