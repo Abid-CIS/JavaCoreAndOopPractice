55d3a6e1f0e84d7d7ddc43af897cf279d4d57ab9
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-05 19:14:36 +0600

    All programs are about java core and OOP practice

--- src/Pattern/Pattern2.java
+++ src/Pattern/Pattern2.java
@@ -2,13 +2,13 @@
 package Pattern;
 import java.util.Scanner;
 
-public class Pattern1 {
+public class Pattern2 {
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter any number ");
         int n = input.nextInt();
-        for (int row = n; row >=1 ; row--) {
+        for (int row = 1; row <= n ; row++) {
             for (int col = 1; col <= row; col++) {
                 System.out.print(" " + col);
             }
