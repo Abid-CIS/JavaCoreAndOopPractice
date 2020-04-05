7ce64d21027f09282247b9991a21ce843e8a408c
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-05 20:20:22 +0600

    All programs are about java core and OOP practice

--- src/ArrayDemo/ArrayDemo1.java
+++ src/ArrayDemo/ArrayDemo1.java
@@ -4,8 +4,19 @@ package ArrayDemo;
 
 public class ArrayDemo1 {
     public static void main(String[] args) {
-        int[] num = new int[10];
-        num[1] = 20;
+       int [] number; // Array Declaration
+       number = new int[5];// Array creation using new keyword
+       number[0] = 10;// Assigning value to array.
+       number[1] = 20;
+       number[2] = 30;
+       number[3] = 40;
+       number[4] = 50;
+       
+       int sum = number[0] + number[1] + number[2] + number[3] + number[4];
+       
+       System.out.println("The Size of array: " + number.length);
+       System.out.println("sum is: " + sum);
+       
         
     }
  
