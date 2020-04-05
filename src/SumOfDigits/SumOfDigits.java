10eef11a15f7d46b9c41b859e585bd1a26f26505
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-05 15:03:52 +0600

    All programs are about java core and OOP practice

--- src/SumOfDigits/SumOfDigits.java
+++ src/SumOfDigits/SumOfDigits.java
@@ -9,7 +9,16 @@ public class SumOfDigits {
         int sum = 0, r, temp, num;
         System.out.println("Enter any number: ");
         num = input.nextInt();
+         
+        temp = num;
         
+        while(temp %10 != 0){
+            
+            r = temp % 10;
+            sum = sum + r;
+            temp = temp/10;
         
+        }
+        System.out.println("The sum is: " + sum);
     }
 }
