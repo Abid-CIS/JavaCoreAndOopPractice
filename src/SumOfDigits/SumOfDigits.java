13f059f034b651ccdc5a6e66b9ea6f83201022d5
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-05 14:43:47 +0600

    All programs are about java core and OOP practice

---
+++ src/SumOfDigits/SumOfDigits.java
@@ -0,0 +1,14 @@
+
+package SumOfDigits;
+import java.util.Scanner;
+
+public class SumOfDigits {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0, r, temp, num;
+        System.out.println("Enter any number: ");
+        num = input.nextInt();
+        
+    }
+}
