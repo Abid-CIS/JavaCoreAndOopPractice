1371f4baf909237592da9ffb91db7f42cabd0263
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-06 22:55:14 +0600

    All programs are about java core and OOP practice

--- src/TwoDimensionalArray/TwoDimensionalArrayDemo.java
+++ src/TwoDimensionalArray/TwoDimensionalArrayDemo.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class TwoDimensionalArrayDemo {
     
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of Row: ");
         int row = input.nextInt();
