29e73e99ac03c87bc30dc555561c1cf893885d55
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-06 14:49:01 +0600

    All programs are about java core and OOP practice

---
+++ src/ForeachLoop/EnhancedForLoopDemo.java
@@ -0,0 +1,13 @@
+
+package ForeachLoop;
+
+
+public class EnhancedForLoopDemo {
+    
+    public static void main(String[] args) {
+        String [] names = {"Apurba", "Shuvra", "Rothosri", "Shama","Rothi", "Sribash"};
+        for (String nam : names) {
+            System.out.println(nam + " ");
+        }
+    }
+}
