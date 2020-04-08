2479aa85e2c48caa9ca17a44052ff96ba5dfecce
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-08 17:00:07 +0600

    All programs are about java core and OOP practice

--- src/TwoDimensionalArray/Array2Demo.java
+++ src/TwoDimensionalArray/Array2Demo.java
@@ -15,14 +15,13 @@ public class Array2Demo {
             }
         }
         System.out.println("Printing value: ");
-        
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 6; j++) {
-                A[i][j] = k;
                 System.out.printf("A[%d][%d]= ", i, j);
-                System.out.println("The value is = " + A[i][j]);
-                k++;
+                System.out.print(" " + A[i][j]);
+                
             }
+            System.out.println("Column complete --->>");
         }
     }
 }
