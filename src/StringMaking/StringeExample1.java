86f351e9d6faf46615d729eb496bc70e87684329
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-10 03:43:50 +0600

    All programs are about java core and OOP practice

--- src/StringMaking/StringeExample1.java
+++ src/StringMaking/StringeExample1.java
@@ -9,7 +9,34 @@ public class StringeExample1 {
         String s1 = "Anisul Islam";
         char[] s3 = {'a', 'b', 'c', 'd'};
         System.out.println(s3);// String is the character type array.
-        String s2 = new String ("Anisul Islam");
+        String s2 = new String ("anisul Islam");
         System.out.println(s1+" "+s2);
+        
+        System.out.println("Length printing of S1 string: ");
+        int len = s1.length();
+        System.out.println("The Length of S1 " + len);
+        //checking Both string are equal or not;
+        // == operator reference Value compare korbe na
+        //->>compare korbe mane object compare korbe.
+        if(s1==s2){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+        //.equals() value compare kore
+        if(s1.equalsIgnoreCase(s2)){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+        //.contains() method 
+        if(s1.contains(s2)){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+        //check korbo kono value ekti string contains korteche kina
+        boolean con = s1.contains("Pakhi");
+        System.out.println(con);
     }
 }
