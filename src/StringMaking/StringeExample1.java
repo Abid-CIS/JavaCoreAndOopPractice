367358c2355d2e8e9d37c2db5fc001f87c1e9807
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-10 03:16:49 +0600

    All programs are about java core and OOP practice

---
+++ src/StringMaking/StringeExample1.java
@@ -0,0 +1,11 @@
+
+package StringMaking;
+
+
+public class StringeExample1 {
+    public static void main(String[] args) {
+        String s1 = "Anisul Islam";
+        String s2 = new String ("Anisul Islam");
+        System.out.println(s1+" "+s2);
+    }
+}
