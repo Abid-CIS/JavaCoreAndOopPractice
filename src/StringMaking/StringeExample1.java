b3dfd73ad98f67a0f80a3ffc37997bd47d6b15a3
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-10 03:20:10 +0600

    All programs are about java core and OOP practice

--- src/StringMaking/StringeExample1.java
+++ src/StringMaking/StringeExample1.java
@@ -4,7 +4,11 @@ package StringMaking;
 
 public class StringeExample1 {
     public static void main(String[] args) {
+        //String is the sequence of character .
+        // String is a array of char.
         String s1 = "Anisul Islam";
+        char[] s3 = {'a', 'b', 'c', 'd'};
+        System.out.println(s3);// String is the character type array.
         String s2 = new String ("Anisul Islam");
         System.out.println(s1+" "+s2);
     }
