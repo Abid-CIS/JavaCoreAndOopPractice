04d4cfb490dab44d6f273281ac676ac8c480892b
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-09 16:28:32 +0600

    All programs are about java core and OOP practice

--- src/ArrayDemo/Array2dAdd.java
+++ src/ArrayDemo/Array2dAdd.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Array2dAdd {
     public static void main(String[] args) {
+        //array -1
         Scanner input = new Scanner(System.in);
         int [][] num1 = new int[4][5];
         //int [][] num2 = new int[4][5];
         
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 5; j++) {
-                System.out.printf("num[%d][%d]= ", i,j);
+                System.out.printf("num1[%d][%d]= ", i,j);
                 num1[i][j] = input.nextInt();
             }
             System.out.println();
         }
         //Value printing: 
-        System.out.println("Printing value: ");
+        System.out.println("Printing value of Array1: ");
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print(" "+ num1[i][j]);
@@ -24,6 +25,32 @@ public class Array2dAdd {
             }
             System.out.println();
         }
+        
+        System.out.println("\n Array2 printing:");
+        //array -2
+        
+        int [][] num2 = new int[4][5];
+       
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("num2[%d][%d]= ", i,j);
+                num2[i][j] = input.nextInt();
+            }
+            System.out.println();
+        }
+        //Value printing: 
+        System.out.println("Printing value of Array2: ");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(" "+ num2[i][j]);
+                
+            }
+            System.out.println();
+        }
+        // Adding 2 Arrays:
+        System.out.println("\n");
+        System.out.println("Adding 2 arrays: ");
         
     }
     
