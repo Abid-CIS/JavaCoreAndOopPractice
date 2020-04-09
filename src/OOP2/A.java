3f997d55cb3412a43fd344daf2b291f7795873f0
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-09 22:41:58 +0600

    All programs are about java core and OOP practice

---
+++ src/OOP2/A.java
@@ -0,0 +1,8 @@
+
+package OOP2;
+
+
+public class A {
+    String name ="Apurba";
+    String address="Kola";
+}
---
+++ src/OOP2/test.java
@@ -0,0 +1,14 @@
+
+package OOP2;
+
+
+public class test {
+    public static void main(String[] args) {
+        A a = new A();
+        String v = a.address;
+        String S = a.name;
+        System.out.println(v+S);
+        
+        
+    }
+}
