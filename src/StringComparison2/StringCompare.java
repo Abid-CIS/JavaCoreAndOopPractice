ac366e02ee396e50a302ccd07d38ccb3045f8ce4
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-10 23:24:27 +0600

    All programs are about java core and OOP practice

--- src/StringComparison2/StringCompare.java
+++ src/StringComparison2/StringCompare.java
@@ -5,6 +5,15 @@ package StringComparison2;
 public class StringCompare {
     
     public static void main(String[] args) {
+        String s1 = "apurba";
+        String s2 = "apurba";
+        String s3 = "ApUrBa";
+        
+        boolean a = s1.equals(s2);// it will return a boolean value
+        boolean  b = s2.equals(s3);
+        System.out.println(" Checking original content of both string.: "+a);// true
+        System.out.println(" Checking original content of both string.: "+b);// false
+        
         
     }
 }
