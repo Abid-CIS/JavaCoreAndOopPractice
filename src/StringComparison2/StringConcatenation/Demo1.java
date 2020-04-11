8e24fec485418cdb3a50b6c88ebbbf297054fe76
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-11 22:38:38 +0600

    All programs are about java core and OOP practice

---
+++ src/StringComparison2/StringConcatenation/Demo1.java
@@ -0,0 +1,20 @@
+
+package StringComparison2.StringConcatenation;
+
+/*
+There are two ways to concate string in java:
+1. By String + concatination Operator.
+2. By concat() method.u
+*/
+public class Demo1 {
+    public static void main(String[] args) {
+        String s1 = "Apurba" + " Debnath";
+        String s2 = " Shuvra";
+        System.out.println("s1 is: " + s1);
+        s1= s1.concat(s2);
+        System.out.println("s1 is now: " + s1);
+        //String concatenation operator (+) can concat not only string but also primitive values:
+        String s = 50+30+"Sachin"+89;
+        System.out.println("s is: " + s);
+    }
+}
