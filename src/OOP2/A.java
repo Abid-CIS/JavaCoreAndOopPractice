4f9f3c49de51811de649d2527eb311052fbff498
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-12 19:49:35 +0600

    All programs are about java core and OOP practice

--- src/OOP2/A.java
+++ src/OOP2/A.java
@@ -4,5 +4,21 @@ package OOP2;
 
 public class A {
     String name ="Apurba";
-    String address="Kola";
+    String address="Kolabagan/Dolphin, Goli";
+    String phn_no = "01745341563";
+    
+    public static void main(String[] args) {
+        A a = new A();
+        a.address = "Rahimullan";
+        a.name = "Sagor Debnath";
+        a.phn_no = "023333333";
+        System.out.println("name is: " + a.address);
+        System.out.println("name is: " + a.phn_no);
+        System.out.println("name is: " + a.name);
+        
+        
+        
+        
+    }
+    
 }
