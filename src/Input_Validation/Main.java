3f5c8853e536d37f1663a0b549a75e8d0219a47f
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-26 17:07:42 +0600

    All programs are about java core and OOP practice

--- src/Input_Validation/Main.java
+++ src/Input_Validation/Main.java
@@ -8,14 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int A, B, C, D, Avg;
-        int count = 0;
+        int count = 1;
         A = 0; B = 0; C = 0; D = 0; Avg = 0;
         do{
-            System.out.println("Enter the first double value:");
-            if(input.hasNextInt()){
-                A = input.nextInt();
-                count++;
-            }
+            A = input.nextInt();
         
         }while(count == 0);
     }
