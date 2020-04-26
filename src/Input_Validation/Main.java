e8e14d6988784bfdc90f9979195abaafce8578da
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-04-26 18:44:57 +0600

    All programs are about java core and OOP practice

--- src/Input_Validation/Main.java
+++ src/Input_Validation/Main.java
@@ -7,12 +7,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int A, B, C, D, Avg;
-        int count = 1;
-        A = 0; B = 0; C = 0; D = 0; Avg = 0;
-        do{
-            A = input.nextInt();
+        int A, B, Avg;
         
-        }while(count == 0);
-    }
+        A = 0; B = 0; Avg = 0;
+        int count = 1;
+        int i = 0;
+        while( i == 0 ){
+        System.out.println("Enter num to A:");
+        A = input.nextInt();
+        do{
+            if(A < 0 || A >10){
+            
+                System.out.println("OOps! please enter 0 to 10");
+                count =2;
+                
+            }
+        }while(count == 1);
+        i++;
+        }  
+   }
+        
 }
+
