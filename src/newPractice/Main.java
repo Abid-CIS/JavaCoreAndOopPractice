847587bab28c80660dd13538bcef9db581dfdb57
Apurba-Debnath-bit <partho.bit999@gmail.com>
2020-06-05 14:59:59 +0600

    Problem Solving of URI

---
+++ src/newPractice/Main.java
@@ -0,0 +1,11 @@
+
+package newPractice;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("The value is: " + a);
+        
+    }
+}
